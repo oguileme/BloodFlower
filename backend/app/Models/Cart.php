27c12e6cae3feb5d3f2bd84820bfba4bf1a897bb
oguileme <guilhermemr.aluno@unipampa.edu.br>
2026-04-13 19:03:58 -0300

    tudo pronto, falta os testes

--- backend/app/Models/Cart.php
+++ backend/app/Models/Cart.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;
 
 class Cart extends Model
 {
     //
+    protected $table = 'cart';
+    
+    protected $fillable = ['quantity']; 
+
+    public function products(): BelongsTo{
+        return $this->belongsTo(Product::class);
+    }
+
+    public function users(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 }
--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use App\Models\Order; // ← adiciona essa linha
+use App\Models\Sizes;
+use App\Models\User;
 
 
 class Product extends Model
@@ -15,13 +17,19 @@ class Product extends Model
 
     //relaçao com a tabela n:n
     public function orders(): BelongsToMany{
-        return $this->belongsToMany(Order::class)
+        return $this->belongsToMany(Order::class, 'order_product' )
                     ->withPivot('quantity')
                     ->withTimestamps();
     }
 
     public function sizes(): BelongsToMany{
-        return $this->belongsToMany(Sizes::class)
+        return $this->belongsToMany(Sizes::class, 'sizes_product')
+                    ->withPivot('quantity')
+                    ->withTimestamps();
+    }
+
+    public function users(): BelongsToMany{
+        return $this->belongsToMany(User::class, 'cart')
                     ->withPivot('quantity')
                     ->withTimestamps();
     }
